6c12a0fcd4c275d3b42a8e3fbc2c57a0f7c2dfd1
Joan Aguilar <j.aguilarmayans@gmail.com>
2023-03-08 14:00:53 -0800

    Add RRT* algorithm.

--- src/motionplanning/algorithms/__init__.py
+++ src/motionplanning/algorithms/__init__.py
@@ -3,3 +3,4 @@ from motionplanning.algorithms.prm import prm, sprm, ksprm
 from motionplanning.algorithms.rrt import rrt, MaxIterError
 from motionplanning.algorithms.prm_star import prm_star, kprm_star
 from motionplanning.algorithms.rrg import rrg, krrg
+from motionplanning.algorithms.rrt_star import rrt_star
